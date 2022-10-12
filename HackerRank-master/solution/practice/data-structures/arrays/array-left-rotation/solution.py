cc6ba38a4fb27d967edc6b3be5478b05818e3329
Chrisler Nunes <69667515+chrislernunes@users.noreply.github.com>
2022-10-13 00:22:12 +0530

    Delete HackerRank-master/solution/practice/data-structures directory

--- HackerRank-master/solution/practice/data-structures/arrays/array-left-rotation/solution.py
+++
@@ -1,6 +0,0 @@
-# We slice the array in two parts at index d, then print them
-# in reverse order.
-
-n, d = map(int, input().split())
-A = list(map(int, input().split()))
-print(*(A[d:] + A[:d])) 
--- HackerRank-master/solution/practice/data-structures/arrays/arrays-ds/solution.py
+++
@@ -1,5 +0,0 @@
-# Just reverse the array right?
-
-N, A = input(), list(map(int, input().split()))
-A.reverse()
-print(*A)
